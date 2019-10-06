c3e9a216dc0d3bb94214889fc8386559c1fea58a
archmantella13 <54756713+archmantella13@users.noreply.github.com>
2019-10-06 09:56:01 -0700

    Create FunctionTemplate.lua

---
+++ FunctionTemplate.lua
@@ -0,0 +1,15 @@
+--Replace name() with what you want to call the function, and add () to the end. Why X inside? 
+--Thats an argumant! Arguments are not required, but fun to have!
+
+function name() 
+  print(X) --Prints waht ever X is.
+end --Ends the function
+
+name(Test)
+--[[ Output:
+
+Test
+
+]]--
+
+--Cool, no?
