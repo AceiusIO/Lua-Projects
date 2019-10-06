c3c178577183ab53d66666037cba41b39703cc36
archmantella13 <54756713+archmantella13@users.noreply.github.com>
2019-10-06 09:19:21 -0700

    Update and rename init.lua to Cmds.lua
    For @Bully-Man so he can learn lua

---
+++ Cmds.lua
@@ -0,0 +1,14 @@
+Hello there!
+
+name = "archmantella13" --Creats a string
+
+dofile("Filename.Fileappend") --Will run the file named this in the running files folder
+
+function functionname(optionalarg) -- Creates a Function
+	dofile("HelloWorld.exe") --Does file HelloWorld.exe
+end
+
+--Print Commands:
+print("Hello"..name..) -- Puts Hello archmantella13 on the termanial
+warn("Warning") -- Prints in a yellow font
+error("Error") -- Prints in a red font with ERROR: text. Example: ERROR: error
