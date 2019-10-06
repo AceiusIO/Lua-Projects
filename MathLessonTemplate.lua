7707c1318fd9ae991d286ac78a0bf9dc07627c4a
archmantella13 <54756713+archmantella13@users.noreply.github.com>
2019-10-06 15:19:19 -0700

    How to use MathLessonTemplate.lua
    Copy all of this into an empty file. Name the file MathLessonBy(Your Username).lua

---
+++ MathLessonTemplate.lua
@@ -0,0 +1,10 @@
+Numb1 = 6
+Numb2 = 5
+---------------------------
+
+function add(X,Y)
+  NumbZ = "Here is where your task starts!"-- Instead of a string, make it so that it adds X, and Y.
+  print("Make this print command print NumbZ!")
+end
+
+add(Numb1,Numb2)
