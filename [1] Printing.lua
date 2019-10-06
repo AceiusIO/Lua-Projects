e705293128fb20a0c8b24930180663c3c8e82bdc
archmantella13 <54756713+archmantella13@users.noreply.github.com>
2019-10-06 09:50:39 -0700

    Create [1] Printing.lua
    WIP

---
+++ [1] Printing.lua
@@ -0,0 +1,14 @@
+--[[ Oh, Hello there!
+I see that you have found your way to this lesson.
+its all about printing!
+]]--
+
+print() --Thats the print command!
+
+--But if it is rmpty, it will not print anything.
+
+print("Hello, World!")
+
+--Ok, good! Now our output will be: Hello, World!
+
+--/Printint Varubles\--
